339e3048aab7d25793ec6efdf33e41f96f9228b0
Paige Tiedeman <paigetiedeman@gmail.com>
2021-10-22 14:58:00 -0700

    Flavors/Create written

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -23,6 +23,7 @@ namespace PierresTreats.Controllers
       _db = db;
     }
 
+    [AllowAnonymous] //anyone can see list of treats
     public async Task<ActionResult> Index()
     {
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
