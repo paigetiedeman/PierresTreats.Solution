5d4d8b713c4f3644a45c42775cb1fbc1d8a63301
Paige Tiedeman <paigetiedeman@gmail.com>
2021-10-22 22:53:05 -0700

    Styling

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -23,12 +23,12 @@ namespace PierresTreats.Controllers
       _db = db;
     }
 
-    // [AllowAnonymous] //anyone can see list of treats
+    [AllowAnonymous] //anyone can see list of treats
     public async Task<ActionResult> Index()
     {
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
       var currentUser = await _userManager.FindByIdAsync(userId);
-      var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).OrderByDescending(model => model.Rating).ToList();
       return View(userTreats);
     }
 
